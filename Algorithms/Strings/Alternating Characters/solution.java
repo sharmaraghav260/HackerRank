22683b3a5d4806565bab70065982ab109458170c
sharmaraghav260 <sharmaraghav260@gmail.com>
2017-08-03 10:31:50 -0600

    fixed indent

--- Algorithms/Strings/Alternating Characters/solution.java
+++ Algorithms/Strings/Alternating Characters/solution.java
@@ -1,9 +1,7 @@
 //Problem: https://www.hackerrank.com/challenges/alternating-characters
 //Java 8
-/*
-Initial thoughts:
-Count the consecutive duplicates and problem is solved.
-*/
+
+/* Initial thoughts: Count the consecutive duplicates and problem is solved. */
 
 import java.io.*;
 import java.util.*;
@@ -11,22 +9,19 @@ import java.util.*;
 public class Solution {
 
   public static void main(String[] args) {
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
-    Scanner input = new Scanner(System.in);
-    int T = input.nextInt();
-    input.nextLine();
+  	/* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+  	Scanner input = new Scanner(System.in);
+  	int T = input.nextInt();
+  	input.nextLine();
 
-    tests:
-    for(int t = 0; t < T; t++)
-    {
-      String s = input.nextLine();
-      int deletions = 0;
-      for(int i = 1; i < s.length(); i++)
-      {
-        if(s.charAt(i) == s.charAt(i-1))
-          deletions += 1;
-      }
-      System.out.println(deletions);
-    }
+  	for(int t = 0; t < T; t++) {
+  		String s = input.nextLine();
+  		int deletions = 0;
+  		for(int i = 1; i < s.length(); i++) {
+  			if(s.charAt(i) == s.charAt(i-1))
+  				deletions += 1;
+  		}
+  		System.out.println(deletions);
+  	}
   }
 }
